0ea9f0c36cd3b1417e914a88870cabb538e0a457
Huai-Che (Acsa) Lu <hc.acsalu@gmail.com>
2015-12-07 21:42:22 -0500

    change ownership.

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,4 +3,4 @@ cd /var/www/emome-server
 virtualenv venv
 . venv/bin/activate
 pip install -r requirements.txt
-chown -R www-data /var/www/emome-server
+chown -R www-data:www-data /var/www/emome-server
