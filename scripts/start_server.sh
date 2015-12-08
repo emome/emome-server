27d07474b5aa6c84c88cf640fca4d307f925ab98
Huai-Che (Acsa) Lu <hc.acsalu@gmail.com>
2015-12-07 19:45:23 -0500

    Update start server script and run port.

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-start uwsgi
+restart uwsgi
--- server.py
+++ server.py
@@ -222,4 +222,4 @@ api.add_resource(History, '/history')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
