ce1c207a8498f8aa9443bbcbcb2d2ba681928b7c
Huai-Che (Acsa) Lu <hc.acsalu@gmail.com>
2015-12-10 09:40:58 -0500

    Use system-site-packages on EC2.

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /var/www/emome-server
-virtualenv venv
+virtualenv --system-site-packages venv
 . venv/bin/activate
 pip install -r requirements.txt
 chown -R www-data:www-data /var/www/emome-server
